8dc96545f69b3783c8250c122700444665a62960
Mike Cecconello <mikececco2000@gmail.com>
2024-06-12 16:54:53 +0200

    Change

--- src/bot/features/any.ts
+++ src/bot/features/any.ts
@@ -38,35 +38,18 @@ feature.on('message', logHandle('command-any'), async (ctx) => {
     const path = file.file_path // file path on Bot API server
 
     if (path) {
-      const config = {
-        audio_url: path,
-      }
-
-      const transcript = await client.transcripts.transcribe(config)
+      const transcript = await client.transcripts.transcribe({ audio_url: path })
 
       console.log(transcript.text)
       if (transcript.text) {
         return ctx.reply(transcript.text)
       }
       console.log('hello')
-      return (ctx.reply('Empty'))
+      return ctx.reply('Empty')
     }
     else {
       console.error('audio_url is undefined')
     }
-    // const voice = ctx.msg.voice
-
-    // const duration = voice.duration // in seconds
-    // await ctx.reply(`Your voice message is ${duration} seconds long.`)
-
-    // const fileId = voice.file_id
-    // await ctx.reply("The file identifier of your voice message is: " + fileId)
-
-    // const file = await ctx.getFile() // valid for at least 1 hour
-    // const path = file.file_path // file path on Bot API server
-    // await ctx.reply("Download your own file again: " + path)
-
-    ctx.reply('You sent a voice message.')
   }
   else if (ctx.message.sticker) {
     ctx.reply('You sent a sticker.')
